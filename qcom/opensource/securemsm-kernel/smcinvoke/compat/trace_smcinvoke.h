NullDebris/android_kernel_nothing_sm7635-modules

qcom: securemsm-kernel: Fix SMCINVOKE_TRACE_INCLUDE_PATH

qcom/opensource/securemsm-kernel/smcinvoke/compat/trace_smcinvoke.h
@@ -491,7 +491,7 @@ TRACE_EVENT(smcinvoke_release,
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
 #define SMCINVOKE_TRACE_INCLUDE_PATH \
-	../../../../vendor/qcom/opensource/securemsm-kernel/smcinvoke/compat
+	../../../sm7635-modules/qcom/opensource/securemsm-kernel/smcinvoke/compat
 #endif
 
 #undef TRACE_INCLUDE_PATH

AmphibianArcade/android_kernel_nothing_sm7750-modules

qcom/opensource/securemsm-kernel/smcinvoke/compat/trace_smcinvoke.h
@@ -491,7 +491,7 @@ TRACE_EVENT(smcinvoke_release,
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
 #define SMCINVOKE_TRACE_INCLUDE_PATH \
-	../../../../vendor/qcom/opensource/securemsm-kernel/smcinvoke/compat
+	../../../sm7750-modules/qcom/opensource/securemsm-kernel/smcinvoke/compat
 #endif
 
 #undef TRACE_INCLUDE_PATH