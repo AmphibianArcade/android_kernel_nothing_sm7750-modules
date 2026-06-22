3fe27885055251091170797c134aca7bbde4e5c4
Alexander Koskovich <akoskovich@pm.me>
2026-06-22 03:04:52 -0400

    focaltech: Add single tap to wake sysfs nodes

--- qcom/opensource/touch-drivers/focaltech_tp/focaltech_core.h
+++ qcom/opensource/touch-drivers/focaltech_tp/focaltech_core.h
@@ -273,6 +273,8 @@ struct fts_ts_data {
     bool fwdbg_support;
     bool gesture_support;   /* gesture enable or disable, default: disable */
     u8 gesture_bmode;       /*gesture buffer mode*/
+    bool single_tap_enabled;
+    bool single_tap_pressed;
     bool palm_to_sleep_support;
 
     int fod_fp_down;
