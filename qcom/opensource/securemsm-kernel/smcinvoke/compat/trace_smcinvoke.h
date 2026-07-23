63945a347a25bfe9a8a1af1345e237fcb32d143f
hiroshi. <hiroshi@tanuki.page>
2026-07-22 22:36:23 -0400

    sm7750 -> sm8750

--- qcom/opensource/securemsm-kernel/smcinvoke/compat/trace_smcinvoke.h
+++ qcom/opensource/securemsm-kernel/smcinvoke/compat/trace_smcinvoke.h
@@ -491,7 +491,7 @@ TRACE_EVENT(smcinvoke_release,
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
 #define SMCINVOKE_TRACE_INCLUDE_PATH \
-	../../../sm7750-modules/qcom/opensource/securemsm-kernel/smcinvoke/compat
+	../../../sm8750-modules/qcom/opensource/securemsm-kernel/smcinvoke/compat
 #endif
 
 #undef TRACE_INCLUDE_PATH
